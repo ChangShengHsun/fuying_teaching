504526c0c602de757b5f11e3bd93b3660224b27e
ivan chang <ic046262@gmail.com>
2026-01-14 10:45:22 +0800

    finish of lesson 1

---
+++ lesson_1/practice_1/boss.cpp
@@ -0,0 +1,19 @@
+/*I got this code, but I don't know what it means. try to understand it 
+and replace the variable with suitable meanings*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+int a=0;
+for(int b=1;b<=20;b++){
+if(b%2==0){
+a+=b;
+cout<<b<<" ";
+}
+}
+cout<<endl;
+cout<<a<<endl;
+return 0;
+}
